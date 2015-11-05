40a12befe47dc9fd6273ab8693b53d7ff50ceae3
“Fitsum <fitsum.ejigu@ge.com>
2015-11-05 16:21:28 -0600

    update on profile

--- Social/public_html/controllers/login.js
+++ Social/public_html/controllers/login.js
@@ -1,31 +1,21 @@
 angular.module('MyApp')
-  .controller('LoginCtrl', function($scope, $location, $auth, toastr, $rootScope, Account) {
+  .controller('LoginCtrl', function($scope, $location, $auth, toastr, $rootScope) {
     $scope.login = function() {
       $auth.login($scope.user)
         .then(function() {
           toastr.success('You have successfully signed in');
-           //here the api call to get user profile
-            $scope.getProfile = function () {
-             Account.getProfile()
-              .then(function (response) {
-               $rootScope.user = response.data;
-                console.log($rootScope.user);
-                 })
-                .catch(function (response) {
-                 toastr.error(response.data.message, response.status);
-                 });
-                 };
-          $location.path('/');
+         
+          $location.path('/profile');
         })
         .catch(function(response) {
-          toastr.error(response.data.message, response.status);
+          toastr.error('The user name or password is incorrect');
         });
     };
     $scope.authenticate = function(provider) {
       $auth.authenticate(provider)
         .then(function() {
           toastr.success('You have successfully signed in with ' + provider);
-          $location.path('/');
+          $location.path('/profile');
         })
         .catch(function(response) {
           toastr.error(response.data.message);
