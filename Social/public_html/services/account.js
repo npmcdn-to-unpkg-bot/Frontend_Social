0b5fd910be2a1aa7d00976440bfa887a5fa592cc
Fitsum <fitsums@gmail.com>
2015-11-09 20:27:34 -0600

    update

--- Social/public_html/services/account.js
+++ Social/public_html/services/account.js
@@ -16,10 +16,10 @@ angular.module('MyApp')
                 updateProfile: function (profileData) {
                        if (window.location.host === 'localhost:8383') {
                       //for local
-                    return $http.get('http://localhost:8080/updateProfile', profileData);  
+                    return $http.put('http://localhost:8080/updateProfile', profileData);  
                     }else{
                            //for cloud
-                    return $http.get('http://tourgoat.cfapps.io/updateProfile', profileData);
+                    return $http.put('http://tourgoat.cfapps.io/updateProfile', profileData);
                     }
                     
                     
