a5c74c8270dd1614e762a57d19b8bac6e029db9b
Weynishet Zula <weynile@gmail.com>
2015-12-13 21:05:28 -0600

    ui change to redirect to login page for if email is not verified

--- Social/public_html/controllers/login.js
+++ Social/public_html/controllers/login.js
@@ -4,10 +4,13 @@ angular.module('MyApp')
          
             $scope.login = function () {
                 $auth.login($scope.user)
-                        .then(function () {
+                        .then(function (response) {
                             //toastr.success('You have successfully signed in');
-
+                           if(!response.data.emailVerified){
+                              $scope.verifyEmail();
+                           }else{
                             $location.path('/profile');
+                        }
                         })
                         .catch(function (response) {
                             //toastr.error(response.data.message, response.status);
@@ -38,5 +41,23 @@ angular.module('MyApp')
                         .catch(function (response) {
                             toastr.error(response.data.message, response.status);
                         });
+            };
+            
+                     //to verify check user email is verifyed or not
+            $scope.verifyEmail = function () {
+                $auth.logout()
+                        .then(function () {
+                            toastr.error('Please verify your email');
+                            $location.path('/login');
+                        });
+
+                $rootScope.emailDiv=true;
+                //Here it send notification messge on user profile page 
+//                setTimeout(function () {
+////                    tjq(".notification-area").append('<div class="info-box block"><span class="close"></span><p style="color:red; text-align: center"> Please verify your email. Click <a style="color: blue" href="">Here</a> to resend email verification</p></div>');
+//                    tjq(".notification-area").append('<div class="info-box block "><span class="close "></span><p style="color:red; text-align: center"> Please verify your email.</p><div class="row "><div class=" form-inline  col-sms-6"><input class="form-control col-sms-4" type="email"  ng-model="email" required placeholder="enter your email"><button class="btn-medium col-sms-2" ng-click="resendEmail()">Resend Email</button></div></div></div>');
+//
+//                }, 0);
+
             };
         });
