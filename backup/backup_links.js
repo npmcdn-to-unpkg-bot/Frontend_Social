403a730aaf5bdd10eb96aec7a7489613d2c3606e
Fitsum <fitsums@gmail.com>
2016-03-05 12:19:58 -0600

    Update om index

---
+++ backup/backup_links.js
@@ -0,0 +1,98 @@
+///* 
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//
+//
+//
+//        <!-- Angular Material style sheet -->
+//        <!--<link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/angular_material/1.0.0/angular-material.min.css">-->
+//
+//        <!--     
+//      
+//    <style type="text/css">
+//            /**
+//             * Hide when Angular is not yet loaded and initialized
+//             */
+//            [ng\:cloak], [ng-cloak], [data-ng-cloak], [x-ng-cloak], .ng-cloak, .x-ng-cloak {
+//              display: none !important;
+//            }
+//        </style>-->
+//
+//
+//
+//        <!--ladda-->
+//        <!--<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/Ladda/0.9.7/ladda.min.css">-->
+//        <!--<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/ladda-bootstrap/0.9.4/ladda-themeless.min.css">-->
+//        <!--<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/ladda-bootstrap/0.9.4/ladda.min.css">-->       
+//
+//
+//
+//        <!---------------------->
+//        <!--    <script src="./vendor/angular.js"></script>
+//            <script src="./vendor/angular-animate.js"></script>
+//            <script src="./vendor/angular-messages.js"></script>
+//            <script src="./vendor/angular-resource.js"></script>
+//            <script src="./vendor/angular-sanitize.js"></script>
+//            <script src="./vendor/angular-ui-router.js"></script>
+//            <script src="./vendor/angular-toastr.tpls.js"></script>
+//            <script src="./vendor/satellizer.js"></script>
+//             <script src="./vendor/jcs-auto-validate.min.js"></script>
+//             <script src="./vendor/ng-tags-input.js"></script>
+//             <script src="./vendor/ngAutocomplete.js"></script>
+//             <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?libraries=places"></script>
+//              <script src="https://code.angularjs.org/1.3.11/angular-route.js"></script>-->
+//        <!--
+//                Third-party Libraries 
+//                <script src="https://code.angularjs.org/1.4.7/angular.min.js"></script>
+//                <script src="https://code.angularjs.org/1.4.7/angular-animate.min.js"></script>
+//                <script src="https://code.angularjs.org/1.4.7/angular-messages.min.js"></script>
+//                <script src="https://code.angularjs.org/1.4.7/angular-resource.min.js"></script>
+//                <script src="https://code.angularjs.org/1.4.7/angular-sanitize.min.js"></script>
+//        
+//                <script src="https://cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.15/angular-ui-router.min.js"></script>
+//        
+//                <script src="https://npmcdn.com/angular-toastr/dist/angular-toastr.tpls.js"></script>
+//                <script src="https://cdnjs.cloudflare.com/ajax/libs/satellizer/0.13.4/satellizer.min.js"></script>
+//        
+//                    <script src="https://cdn.rawgit.com/jonsamwell/angular-auto-validate/master/dist/jcs-auto-validate.js"></script>
+//                <script src="https://cdnjs.cloudflare.com/ajax/libs/ng-tags-input/3.0.0/ng-tags-input.min.js"></script>
+//                <script src="vendor/ngAutocomplete.js"></script>
+//                <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?libraries=places"></script>
+//                <script src="https://code.angularjs.org/1.3.11/angular-route.js"></script>
+//        
+//                Ladda spinner
+//                <script src="//cdnjs.cloudflare.com/ajax/libs/spin.js/2.0.1/spin.js"></script>
+//                <script src="//cdnjs.cloudflare.com/ajax/libs/Ladda/0.9.4/ladda.min.js"></script>
+//                <script src="vendor/angular-ladda.js"></script>-->
+//
+//        <!--<script src="https://code.angularjs.org/1.3.11/angular-route.js"></script>-->
+//
+//
+//        <!-- Load Angular scripts-->
+//
+//        <!--<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.17/angular.min.js"></script>-->
+//        <!--<script src="./vendor/ng-flow-standalone.min.js"></script>-->
+//
+//        <!-- jQuery -->
+//        <!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8/jquery.min.js"></script>-->
+//
+//        <!-- Verify.js (with Notify.js included) -->
+//        <!--<script src="//raw.github.com/jpillora/verifyjs/gh-pages/dist/verify.notify.min.js"></script>-->
+//
+//        <!-- CSS for IE -->
+//        <!--[if lte IE 9]>
+//            <link rel="stylesheet" type="text/css" href="css/ie.css" />
+//        <![endif]-->
+//
+//
+//        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+//        <!--[if lt IE 9]>
+//          <script type='text/javascript' src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+//          <script type='text/javascript' src="http://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js"></script>
+//        <![endif]-->
+//
+//<!--         Javascript Page Loader 
+//        <script type="text/javascript" src="js/pace.min.js" data-pace-options='{ "ajax": false }'></script>
+//        <script type="text/javascript" src="js/page-loading.js"></script>-->
