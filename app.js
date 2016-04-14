41eb296c13a7a8e63bfdf909fa7745ff070d85d8
Fitsum <fitsums@gmail.com>
2016-04-13 21:06:34 -0500

    admin url fixed video player added

--- app.js
+++ app.js
@@ -1,198 +1,231 @@
-var app = angular.module('MyApp', ['ngResource', 'ngMessages', 'ngAnimate','toastr', 'ui.router', 'satellizer', 'jcs-autoValidate', 'ngMaterial', 'ngTagsInput', 'ngAutocomplete', 'angular-ladda','datatables', 'ngResource','ui.bootstrap'])
-        .config(function ($stateProvider, $urlRouterProvider, $authProvider, $locationProvider) {
-            $stateProvider
-                    .state('index', {
-                        url: '/index',
-                        controller: 'homeCtrl',
-                        templateUrl: 'pages/home.html'
-
-                    })
-                    .state('home', {
-                        url: '/',
-                        controller: 'homeCtrl',
-                        templateUrl: 'pages/home.html'
-                    })
-                    .state('login', {
-                        url: '/login',
-                        templateUrl: 'pages/login.html',
-                        controller: 'LoginCtrl',
-                        resolve: {
-                            skipIfLoggedIn: skipIfLoggedIn
-                        },
-                         params: {
-                               emailVerified: null
-                         }
-                    })
-                    .state('signup', {
-                        url: '/signup',
-                        templateUrl: 'pages/signup.html',
-                        controller: 'SignupCtrl',
-                        resolve: {
-                            skipIfLoggedIn: skipIfLoggedIn
-                        }
-                    })
-                    .state('logout', {
-                        url: '/logout',
-                        template: null,
-                        controller: 'LogoutCtrl'
-                    })
-                    .state('profile', {
-                        url: '/profile',
-                        controller: 'ProfileCtrl',
-                        templateUrl: 'pages/profile.html',
-                        resolve: {
-                            loginRequired: loginRequired
-                        }
-                    })
-                    .state('term', {
-                        url: '/term',
-                        templateUrl: 'pages/term.html'
-
-                    }).state('becometourguide', {
-                url: '/becometourguide',
-                templateUrl: 'pages/becometourguide.html',
-                controller: 'becomeTourguideCtrl'
+var app = angular.module('MyApp',
+        [
+            'ngResource',
+            'ngMessages',
+            'ngAnimate',
+            'toastr',
+            'ui.router',
+            'satellizer',
+            'jcs-autoValidate',
+            'ngMaterial',
+            'ngTagsInput',
+            'ngAutocomplete',
+            'angular-ladda',
+            'datatables',
+            'ngResource',
+            'ui.bootstrap',
+            "ngSanitize",
+            "com.2fdevs.videogular",
+            "com.2fdevs.videogular.plugins.poster"
 
 
-            }).state('passwordReset', {
-                url: '/passwordReset',
-                controller: 'PasswordResetCtrl',
-                templateUrl: 'pages/passwordReset.html'
+        ]);
+app.config(function ($stateProvider, $urlRouterProvider, $authProvider, $locationProvider) {
+    $stateProvider
+            .state('index', {
+                url: '/index',
+                controller: 'homeCtrl',
+                templateUrl: 'pages/home.html'
 
-            }).state('newPassword', {
-                url: '/newPassword?emailAddress=${email}',
-                controller: function ($scope, $stateParams) {
-                    // get the id
-                    $scope.emailAddress = $stateParams.email;
-
-                    // get the location
-                    $scope.newPassword = true;
-                    $scope.resetPassword = false;
-                },
-                templateUrl: 'pages/passwordReset.html'
-
-            }).state('dashboard', {
-                url: '/dashboard',
-                        controller: 'AngularWayChangeDataCtrl',
-                templateUrl: 'pages/admin-dashboard.html',
+            })
+            .state('home', {
+                url: '/',
+                controller: 'homeCtrl',
+                templateUrl: 'pages/home.html'
+            })
+            .state('login', {
+                url: '/login',
+                templateUrl: 'pages/login.html',
+                controller: 'LoginCtrl',
                 resolve: {
-                           adminloginRequired: adminloginRequired
-                        }
-
-            }).state('list', {
-                url: '/list',
-                controller: 'listViewCtrl',
-                templateUrl: 'pages/tourguide-list-view.html'
-
-            }).state('listDetail', {
-                url: '/listDetail',
-                controller: 'listDetailCtrl',
-                templateUrl: 'pages/list-detail-view.html'
-
-            }).state('booking', {
-                url: '/booking',
-                controller: 'bookingCtrl',
-                templateUrl: 'pages/booking.html'
-
-            }).state('admin', {
-                url: '/admin',
-                controller: 'adminCtrl',
-                templateUrl: 'pages/adminLogin.html'
-
-            }).state('emailVerification', {
-                url: '/emailVerification',
-               controller: function ($state) {
-                     $state.go('login',{emailVerified :true});
+                    skipIfLoggedIn: skipIfLoggedIn
+                },
+                params: {
+                    emailVerified: null
                 }
-                 
-            });
+            })
+            .state('signup', {
+                url: '/signup',
+                templateUrl: 'pages/signup.html',
+                controller: 'SignupCtrl',
+                resolve: {
+                    skipIfLoggedIn: skipIfLoggedIn
+                }
+            })
+            .state('logout', {
+                url: '/logout',
+                template: null,
+                controller: 'LogoutCtrl'
+            })
+            .state('profile', {
+                url: '/profile',
+                controller: 'ProfileCtrl',
+                templateUrl: 'pages/profile.html',
+                resolve: {
+                    loginRequired: loginRequired
+                }
+            })
+            .state('term', {
+                url: '/term',
+                templateUrl: 'pages/term.html'
 
-            $urlRouterProvider.otherwise('/');
+            }).state('becometourguide', {
+        url: '/becometourguide',
+        templateUrl: 'pages/becometourguide.html',
+        controller: 'becomeTourguideCtrl'
 
-            $authProvider.withCredentials = false;
 
-            if (window.location.host === 'localhost:8383') {
-                //local dev
-                $authProvider.baseUrl = 'http://localhost:8080/';
-                $authProvider.facebook({
-                    clientId: '959764637427221',
-                    redirectUri: (window.location.origin || window.location.protocol + '//' + window.location.host) + '/',
-                    url: 'http://localhost:8080/auth/facebook',
-                    scope: ['email','user_birthday']
+    }).state('passwordReset', {
+        url: '/passwordReset',
+        controller: 'PasswordResetCtrl',
+        templateUrl: 'pages/passwordReset.html'
+
+    }).state('newPassword', {
+        url: '/newPassword?emailAddress=${email}',
+        controller: function ($scope, $stateParams) {
+            // get the id
+            $scope.emailAddress = $stateParams.email;
+
+            // get the location
+            $scope.newPassword = true;
+            $scope.resetPassword = false;
+        },
+        templateUrl: 'pages/passwordReset.html'
+
+    }).state('dashboard', {
+        url: '/dashboard',
+        controller: 'AngularWayChangeDataCtrl',
+        templateUrl: 'pages/admin-dashboard.html',
+        resolve: {
+            adminloginRequired: adminloginRequired
+        }
+
+    }).state('list', {
+        url: '/list',
+        controller: 'listViewCtrl',
+        templateUrl: 'pages/tourguide-list-view.html'
+
+    }).state('listDetail', {
+        url: '/listDetail',
+        controller: 'listDetailCtrl',
+        templateUrl: 'pages/list-detail-view.html'
+
+    }).state('booking', {
+        url: '/booking',
+        controller: 'bookingCtrl',
+        templateUrl: 'pages/booking.html'
+
+    }).state('user_admin', {
+        url: '/user_admin',
+        controller: 'adminCtrl',
+        templateUrl: 'pages/adminLogin.html',
+         resolve: {
+            adminUrlRequeste: adminUrlRequeste
+        }
+
+    }).state('emailVerification', {
+        url: '/emailVerification',
+        controller: function ($state) {
+            $state.go('login', {emailVerified: true});
+        }
+
+    });
+
+    $urlRouterProvider.otherwise('/');
+
+    $authProvider.withCredentials = false;
+
+    if (window.location.host === 'localhost:8383') {
+        //local dev
+        $authProvider.baseUrl = 'http://localhost:8080/';
+        $authProvider.facebook({
+            clientId: '959764637427221',
+            redirectUri: (window.location.origin || window.location.protocol + '//' + window.location.host) + '/',
+            url: 'http://localhost:8080/auth/facebook',
+            scope: ['email', 'user_birthday']
 //                    scope: ['email']
-                });
-
-                $authProvider.google({
-                    clientId: '1063684996500-2gk0ejdiq02b68thlnggavb8arfmtobu.apps.googleusercontent.com',
-                    url: 'http://localhost:8080/auth/google',
-                    redirectUri: (window.location.origin || window.location.protocol + '//' + window.location.host) + '/'
-
-                });
-            } 
-            if(window.location.host === 'app-tourgoat.rhcloud.com'){
-                // cloud QA
-                $authProvider.baseUrl = 'http://tourgoat.cfapps.io/';
-                $authProvider.facebook({
-                    clientId: '959764637427221',
-                    redirectUri: (window.location.origin || window.location.protocol + '//' + window.location.host) + '/',
-                    url: 'http://tourgoat.cfapps.io/auth/facebook',
-                    scope: ['email','user_birthday']
-//                    scope: ['email']
-                });
-                $authProvider.google({
-                    clientId: '1063684996500-2gk0ejdiq02b68thlnggavb8arfmtobu.apps.googleusercontent.com',
-                    url: 'http://tourgoat.cfapps.io/auth/google',
-                    redirectUri: (window.location.origin || window.location.protocol + '//' + window.location.host) + '/'
-                });
-            }
-            if(window.location.host === 'tourgoat.com.s3-website-us-west-2.amazonaws.com' || window.location.host === 'http://tourgoat.com' || window.location.host === 'http://www.tourgoat.com'){
-                // cloud prod
-                $authProvider.baseUrl = 'http://tourgoatapp-env.us-west-2.elasticbeanstalk.com/';
-                $authProvider.facebook({
-                    clientId: '959764637427221',
-                    redirectUri: (window.location.origin || window.location.protocol + '//' + window.location.host) + '/',
-                    url: 'http://tourgoatapp-env.us-west-2.elasticbeanstalk.com/auth/facebook',
-                    scope: ['email','user_birthday' ]
-//                    scope: ['email']
-                });
-                $authProvider.google({
-                    clientId: '1063684996500-2gk0ejdiq02b68thlnggavb8arfmtobu.apps.googleusercontent.com',
-                    url: 'http://tourgoatapp-env.us-west-2.elasticbeanstalk.com/auth/google',
-                    redirectUri: (window.location.origin || window.location.protocol + '//' + window.location.host) + '/'
-                });
-            }
-
-
-
-            function skipIfLoggedIn($q, $auth) {
-                var deferred = $q.defer();
-                if ($auth.isAuthenticated()) {
-                    deferred.reject();
-                } else {
-                    deferred.resolve();
-                }
-                return deferred.promise;
-            }
-
-            function loginRequired($q, $location, $auth) {
-                var deferred = $q.defer();
-                if ($auth.isAuthenticated()) {
-                    deferred.resolve();
-                } else {
-                    $location.path('/login');
-                }
-                return deferred.promise;
-            }
-             function adminloginRequired($q, $location, $auth) {
-                var deferred = $q.defer();
-                if ($auth.isAuthenticated()) {
-                    deferred.resolve();
-                } else {
-                    $location.path('/admin');
-                }
-                return deferred.promise;
-            }
         });
+
+        $authProvider.google({
+            clientId: '1063684996500-2gk0ejdiq02b68thlnggavb8arfmtobu.apps.googleusercontent.com',
+            url: 'http://localhost:8080/auth/google',
+            redirectUri: (window.location.origin || window.location.protocol + '//' + window.location.host) + '/'
+
+        });
+    }
+    if (window.location.host === 'app-tourgoat.rhcloud.com') {
+        // cloud QA
+        $authProvider.baseUrl = 'http://tourgoat.cfapps.io/';
+        $authProvider.facebook({
+            clientId: '959764637427221',
+            redirectUri: (window.location.origin || window.location.protocol + '//' + window.location.host) + '/',
+            url: 'http://tourgoat.cfapps.io/auth/facebook',
+            scope: ['email', 'user_birthday']
+//                    scope: ['email']
+        });
+        $authProvider.google({
+            clientId: '1063684996500-2gk0ejdiq02b68thlnggavb8arfmtobu.apps.googleusercontent.com',
+            url: 'http://tourgoat.cfapps.io/auth/google',
+            redirectUri: (window.location.origin || window.location.protocol + '//' + window.location.host) + '/'
+        });
+    }
+    if (window.location.host === 'tourgoat.com.s3-website-us-west-2.amazonaws.com' || window.location.host === 'http://tourgoat.com' || window.location.host === 'http://www.tourgoat.com') {
+        // cloud prod
+        $authProvider.baseUrl = 'http://tourgoatapp-env.us-west-2.elasticbeanstalk.com/';
+        $authProvider.facebook({
+            clientId: '959764637427221',
+            redirectUri: (window.location.origin || window.location.protocol + '//' + window.location.host) + '/',
+            url: 'http://tourgoatapp-env.us-west-2.elasticbeanstalk.com/auth/facebook',
+            scope: ['email', 'user_birthday']
+//                    scope: ['email']
+        });
+        $authProvider.google({
+            clientId: '1063684996500-2gk0ejdiq02b68thlnggavb8arfmtobu.apps.googleusercontent.com',
+            url: 'http://tourgoatapp-env.us-west-2.elasticbeanstalk.com/auth/google',
+            redirectUri: (window.location.origin || window.location.protocol + '//' + window.location.host) + '/'
+        });
+    }
+
+
+
+    function skipIfLoggedIn($q, $auth) {
+        var deferred = $q.defer();
+        if ($auth.isAuthenticated()) {
+            deferred.reject();
+        } else {
+            deferred.resolve();
+        }
+        return deferred.promise;
+    }
+
+    function loginRequired($q, $location, $auth) {
+        var deferred = $q.defer();
+        if ($auth.isAuthenticated()) {
+            deferred.resolve();
+        } else {
+            $location.path('/login');
+        }
+        return deferred.promise;
+    }
+    function adminloginRequired($q, $location, $auth) {
+        var deferred = $q.defer();
+        if ($auth.isAuthenticated()) {
+            deferred.resolve();
+        } else {
+            $location.path('/admin');
+        }
+        return deferred.promise;
+    }
+      function adminUrlRequeste ($q, $location, $auth) {
+
+        if ($auth.isAuthenticated()) {
+            $auth.logout();
+            $location.path('/user_admin');
+        } else{
+            $location.path('/user_admin');
+        }
+    }
+});
 app.run(function (defaultErrorMessageResolver) {
     defaultErrorMessageResolver.getErrorMessages().then(function (errorMessages) {
         errorMessages['minPassword'] = 'Please enter at least 6 characters long ';
--- controllers/becomeTourguide.js
+++ controllers/becomeTourguide.js
@@ -1,8 +1,15 @@
-angular.module('MyApp')
-        .controller('becomeTourguideCtrl', function ( $rootScope) {
-            //this is make display on home page content 
-//            $rootScope.homePageContent = false;
-            //This script run home page slider
-           
 
-        });
+angular.module('MyApp')
+  .controller('becomeTourguideCtrl', function ($scope, $sce) {
+    $scope.config = {
+      sources: [
+          {src: $sce.trustAsResourceUrl('http://static.videogular.com/assets/videos/videogular.mp4'), type: 'video/mp4'}
+      ],
+      useNativeControls: true,
+      posterImage: 'http://www.videogular.com/assets/images/videogular.png',
+      style:'http://www.videogular.com/styles/themes/default/latest/videogular.css'
+    };
+});
+
+
+
